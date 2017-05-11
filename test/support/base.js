5e01247713f2f37c87eaa2db88de919ee6f5691b
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-05-11 23:31:11 +0100

    Update sinon + remove unnecessary sinon-as-promised

--- test/support/base.js
+++ test/support/base.js
@@ -3,11 +3,9 @@ import sinon from 'sinon';
 import chai, {expect} from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import chaiSinon from 'sinon-chai';
-import sinonAsPromised from 'sinon-as-promised';
 
 process.env.NODE_ENV = 'test';
 
-sinonAsPromised(Bluebird);
 chai.use(chaiAsPromised);
 chai.use(chaiSinon);
 chai.should();
