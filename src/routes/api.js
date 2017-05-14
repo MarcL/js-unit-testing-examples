ed2256078319137956d4d90296a6b530115bf592
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-05-14 21:48:24 +0100

    Refactor API names for future use

--- src/routes/api.js
+++ src/routes/api.js
@@ -8,14 +8,14 @@ router.get('/', (request, response) => {
 });
 
 // TODO
-// - Add GET for all data /data - return data + 200
+// - Add GET for all data /events - return data + 200
 router.get('/events', eventsGetAll);
-//router.get('/data/:id', dataGetById);
+//router.get('/events/:id', dataGetById);
 
-// - Add POST to add new data /data - return 201
-// - Add GET for single data element /data/:id -200 / 404
-// - Add PUT to update single data element /data/:id - 200/204 (no content)/4040
-// - Add DELETE to delete single data element /data/:id - 200/404
+// - Add POST to add new data /events - return 201
+// - Add GET for single data element /events/:id -200 / 404
+// - Add PUT to update single data element /events/:id - 200/204 (no content)/4040
+// - Add DELETE to delete single data element /events/:id - 200/404
 // - Add authentication - basic auth - 'auth' -> 'key:' (no password) + base64 encoded - 401 if failed
 
 export default router;
